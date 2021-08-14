925f51cac682b7c7c5779892096ab6b76cb8f5b3
Masud8851 <masud1971.cse@gmail.com>
2021-08-14 13:40:18 +0600

    DOM Events

--- js/script.js
+++ js/script.js
@@ -1,14 +1,9 @@
 // console.log('Connected.')
 
 // h2 tag lightBlue
-// let header2 = document.getElementsByTagName('h2');
-
-// for (let i = 0; i < header2.length; i++) {
-//     header2[i].style.color = 'lightblue';
-// }
-
 let secondHeader = document.getElementsByTagName('h2');
 for(let i=0; i<secondHeader.length; i++)
 {
     secondHeader[i].style.color = 'blue';
 }
+
