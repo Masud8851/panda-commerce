4056c0bbc809b7be6ee9ac4461b9b9b7061f704c
Masud8851 <masud1971.cse@gmail.com>
2021-08-14 14:24:24 +0600

    DOM Events

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 // console.log('Connected.')
 
-// h2 tag lightBlue
+// যতগুলা ইলিমেন্টে h2 ট্যাগ আছে সেগুলাকে সিলেক্ট করে তার লেখাগুলার কালার lightblue করেদিতে পারো কিনা। 
 let secondHeader = document.getElementsByTagName('h2');
 for(let i=0; i<secondHeader.length; i++)
 {
     secondHeader[i].style.color = 'blue';
 }
+
+// backpack আইডি দিয়ে একটা সেকশন আছে। সেই সেকশনের ব্যাকগ্রাউন্ড কালার tomato করে দিতে পারো কিনা। 
 
